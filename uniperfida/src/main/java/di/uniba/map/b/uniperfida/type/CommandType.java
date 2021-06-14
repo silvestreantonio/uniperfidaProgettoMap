1d7ee86b52eb179f2937bc84633c5eaf2db73dad
Antonio Silvestre <61579787+silvestreantonio@users.noreply.github.com>
2021-06-14 18:34:19 +0200

    Update CommandType.java

--- uniperfida/src/main/java/di/uniba/map/b/uniperfida/type/CommandType.java
+++ uniperfida/src/main/java/di/uniba/map/b/uniperfida/type/CommandType.java
@@ -12,5 +12,5 @@ package di.uniba.map.b.uniperfida.type;
 
 // comandi che il parser puo capire in maniera generica (tipo enumerativo)
 public enum CommandType {
-    END, INVENTORY, NORTH, SOUTH, EAST, WEST, OPEN, PUSH, PICK_UP, LEAVE, TALK, USE, LOOK, GO_UP, GO_DOWN, HELP, READ
+    END, INVENTORY, NORTH, SOUTH, EAST, WEST, OPEN, PUSH, PICK_UP, LEAVE, TALK, USE, LOOK, GO_UP, GO_DOWN, HELP, READ, ENTER
 }
